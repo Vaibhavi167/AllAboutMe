4846ff470c159824e4412072ea3bd0ae671bdf5c
Vaibhavi Bandaru <vaibhavi.bandaru7@gmail.com>
2025-07-09 13:05:10 -0400

    Initial Commit

---
+++ AllAboutMe/AllAboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllAboutMeApp.swift
+//  AllAboutMe
+//
+//  Created by Vaibhavi Bandaru on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct AllAboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
