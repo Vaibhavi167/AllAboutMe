f9020d18d76f449357ed0c4bc6e7a113df9d1ba3
Vaibhavi Bandaru <vaibhavi.bandaru7@gmail.com>
2025-07-09 13:11:16 -0400

    Copied code for all about me layout

--- AllAboutMe/ContentView.swift
+++ AllAboutMe/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  AllAboutMe
+//  GitPractice
 //
 //  Created by Vaibhavi Bandaru on 7/9/25.
 //
@@ -9,13 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, friends!")
+        ZStack
+        {
+            //.background(Color.purple)
+            //.background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+            
+            
+            VStack
+            {
+                Text("All About Vaibhavi Bandaru!")
+                
+                ZStack
+                {
+                    Rectangle()
+                        .fill(Color.purple)
+                        .frame(width: 375, height: 235, alignment: .center)
+                        //.rotationEffect(.degrees(45.0))
+                    
+                    HStack
+                    {
+                        Image("dance1")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .padding(.all, 18.0)
+                        Text("This is one of my pictures!")
+                            .padding(.trailing, 10.0)
+                        
+                    }
+                }
+                
+                
+                ZStack
+                {
+                    Rectangle()
+                        .fill(Color.purple)
+                        .frame(width: 375, height: 310, alignment: .center)
+                        //.rotationEffect(.degrees(45.0))
+                    
+                    HStack
+                    {
+                        Image("dance2")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .padding(.all, 18.0)
+                        Text("This is another picture!")
+                            .padding(.trailing, 10.0)
+                        
+                    }
+                }
+                
+            }
         }
-        .padding()
+        
+        .padding(.all, 15.0)
     }
 }
 
